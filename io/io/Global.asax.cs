2d45c3cac2e27552e5b2e83ceea0b43288262a11
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2020-09-16 06:11:38 +0700

    app router

--- io/io/Global.asax.cs
+++ io/io/Global.asax.cs
@@ -2,39 +2,107 @@
 using System.IO;
 using System.Linq;
 using System.Web;
+using Newtonsoft.Json;
 using Microsoft.ClearScript.V8;
 using System.Collections.Generic;
+using System.Collections.Concurrent;
 
 namespace io
 {
-    public class Global : System.Web.HttpApplication
+    public class oSite
     {
-        static oSite[] mSites = new oSite[] { };
-        static System.Collections.Concurrent.ConcurrentDictionary<string, oSite> mDomains
-            = new System.Collections.Concurrent.ConcurrentDictionary<string, oSite>();
+        public int id { set; get; }
+        public string name { set; get; }
+        public string[] domains { set; get; }
+        public string[] pages { set; get; }
+    }
 
-        static System.Collections.Concurrent.ConcurrentDictionary<string, string> mTokens
-            = new System.Collections.Concurrent.ConcurrentDictionary<string, string>();
+    public class oPage
+    {
+        public int id { set; get; }
+        public int site_id { set; get; }
+        public string key { set; get; }
+        public string path { set; get; }
+        public string theme { set; get; }
+        public bool login { set; get; }
+    }
+
+    public static class self
+    {
+        static string[] mPaths = new string[] { };
+        static ConcurrentDictionary<int, oSite> mSites = new ConcurrentDictionary<int, oSite>();
+        static ConcurrentDictionary<int, oPage> mPages = new ConcurrentDictionary<int, oPage>();
+        static ConcurrentDictionary<string, int> mDomainSite = new ConcurrentDictionary<string, int>();
+        static ConcurrentDictionary<int, int> mPathPage = new ConcurrentDictionary<int, int>();
+
+        static ConcurrentDictionary<string, string> mTokens = new ConcurrentDictionary<string, string>();
 
         static string[] mPass = new string[] { };
 
-        protected void Application_Start(object sender, EventArgs e)
+        public static void app_loadConfig(HttpApplication app)
         {
-            string file = Server.MapPath("~/io/config.json");
+            List<string> listPaths = new List<string>() { };
+            string file = app.Server.MapPath("~/io/config.json");
             if (File.Exists(file))
             {
-                var lines = File.ReadAllLines(file).Where(x => x.Trim().Length > 0)
-                    .Select(x => x.Split(';')).Where(x => x.Length == 2).GroupBy(x => x[0]).Select(x => x.Last()).ToArray();
-                for (var i = 0; i < lines.Length; i++)
-                    try { mDomains.TryAdd(lines[i][0], int.Parse(lines[i][1])); } catch { }
+                string json = File.ReadAllText(file);
+                oSite[] sites = new oSite[] { };
+                try
+                {
+                    sites = JsonConvert.DeserializeObject<oSite[]>(json);
+                    var ss = sites.GroupBy(x => x.id).Select(x => x.Last()).ToArray();
+                    foreach (var site in ss)
+                    {
+                        mSites.TryAdd(site.id, site);
+
+                        string[] domains = new string[] { };
+                        if (site.domains != null && site.domains.Length > 0)
+                            domains = site.domains.Distinct().ToArray();
+                        foreach (string domain in domains) mDomainSite.TryAdd(domain, site.id);
+
+                        var pages = new string[][] { };
+                        if (site.pages != null && site.pages.Length > 0)
+                            pages = site.pages.Distinct()
+                                .Select(x => x.Split('.'))
+                                .Where(x => x.Length == 3)
+                                .ToArray();
+
+                        foreach (var ps in pages)
+                        {
+                            var p = new oPage()
+                            {
+                                id = mPages.Count + 1,
+                                key = string.Join(".", ps),
+                                login = ps[2] == "1",
+                                site_id = site.id,
+                                path = ps[0],
+                                theme = ps[1]
+                            };
+                            mPages.TryAdd(p.id, p);
+
+                            foreach (var domain in domains)
+                            {
+                                string fullPath = string.Format("{0}/{1}", domain, ps[0]);
+                                mPathPage.TryAdd(listPaths.Count, p.id);
+                                listPaths.Add(fullPath);
+                            }
+                        }
+                    }
+                }
+                catch { }
             }
 
-            string fLogin = Server.MapPath("~/data/login.txt");
+            mPaths = listPaths.ToArray();
+        }
+
+        public static void user_loadPassword(HttpApplication app)
+        {
+            string fLogin = app.Server.MapPath("~/data/login.txt");
             if (File.Exists(fLogin))
                 mPass = File.ReadAllLines(fLogin).Where(x => x.Trim().Length > 0).ToArray();
         }
 
-        protected string createToken(string username)
+        private static string user_createToken(string username)
         {
             var jsSerializer = new System.Web.Script.Serialization.JavaScriptSerializer();
             string token = jsSerializer.Serialize(new
@@ -50,7 +118,7 @@ namespace io
             return sBase64;
         }
 
-        protected void responseWrite(object item)
+        private static void response_Write(object item)
         {
             if (item != null)
             {
@@ -65,34 +133,8 @@ namespace io
             }
         }
 
-        protected void testClearScript()
-        {
-            string text = "", js = "";
+        public static void request_Router(HttpApplication app) {
 
-            using (var engine = new V8ScriptEngine())
-            {
-                string f = Server.MapPath("~/public/vue.min.js");
-                js = File.ReadAllText(f) + Environment.NewLine + @"
-                    function test(html) { var template = Vue.compile(html); return template.render.toString(); }
-                ";
-                engine.Execute(js);
-                string html = @"<div>
-                    <h1>{{title}}</h1>
-                    <input type=""text"" v-model=""title"" id=""fname"" name=""fname""><br>
-                    <special-article :article-title=""title""></special-article><br>
-                </div>";
-                text = engine.Script.test(html) as string;
-            }
-
-            HttpContext.Current.Response.Clear();
-            HttpContext.Current.Response.ContentType = "text/plain";
-            HttpContext.Current.Response.Write(text);
-            HttpContext.Current.Response.Flush();
-            HttpContext.Current.Response.Close();
-        }
-
-        protected void Application_BeginRequest(object sender, EventArgs e)
-        {
             Uri url = HttpContext.Current.Request.Url;
             try
             {
@@ -113,35 +155,31 @@ namespace io
                                 HttpContext.Current.RewritePath("~/public/admin.html");
                                 break;
                             case "test-v8":
-                                testClearScript();
+                                //testClearScript();
                                 break;
                             case "test":
                                 string site = HttpContext.Current.Request.QueryString["site"];
                                 if (string.IsNullOrWhiteSpace(site)) site = "index";
-                                pathFile = Server.MapPath("~/test/" + site + ".html");
-                                if (System.IO.File.Exists(pathFile))
-                                {
+                                pathFile = app.Server.MapPath("~/test/" + site + ".html");
+                                if (File.Exists(pathFile))
                                     HttpContext.Current.RewritePath("~/test/" + site + ".html");
-                                }
                                 else
-                                {
-                                    responseWrite(new { Ok = false, Message = "Cannot find " + pathFile });
-                                }
+                                    response_Write(new { Ok = false, Message = "Cannot find " + pathFile });
                                 break;
                             default:
-                                if (mDomains.ContainsKey(domain))
-                                {
-                                    int id = 0;
-                                    if (mDomains.TryGetValue(domain, out id) && id > 0)
-                                    {
-                                        string fileName = (path.Length == 0 ? "index.html" : path);
-                                        if (!fileName.EndsWith(".html")) fileName += ".html";
-                                        file = "~/public/" + id.ToString() + "/" + fileName;
-                                        pathFile = Server.MapPath(file);
-                                        if (!System.IO.File.Exists(pathFile)) file = string.Empty;
-                                    }
-                                    if (file.Length > 0) HttpContext.Current.RewritePath(file);
-                                }
+                                ////if (mDomains.ContainsKey(domain))
+                                ////{
+                                ////    int id = 0;
+                                ////    if (mDomains.TryGetValue(domain, out id) && id > 0)
+                                ////    {
+                                ////        string fileName = (path.Length == 0 ? "index.html" : path);
+                                ////        if (!fileName.EndsWith(".html")) fileName += ".html";
+                                ////        file = "~/public/" + id.ToString() + "/" + fileName;
+                                ////        pathFile = app.Server.MapPath(file);
+                                ////        if (!System.IO.File.Exists(pathFile)) file = string.Empty;
+                                ////    }
+                                ////    if (file.Length > 0) HttpContext.Current.RewritePath(file);
+                                ////}
                                 break;
                         }
                         break;
@@ -155,7 +193,7 @@ namespace io
                             case "login":
                                 try
                                 {
-                                    var stream = new System.IO.StreamReader(Request.InputStream);
+                                    var stream = new System.IO.StreamReader(app.Request.InputStream);
                                     text = stream.ReadToEnd();
                                     json = HttpUtility.UrlDecode(text);
 
@@ -176,12 +214,12 @@ namespace io
                                     result = @"{""Ok"":false}";
                                     if (ok)
                                     {
-                                        token = createToken(username);
+                                        token = user_createToken(username);
                                         string temp = string.Empty;
                                         if (mTokens.ContainsKey(username)) mTokens.TryRemove(username, out temp);
 
                                         string userJson = "{}";
-                                        string fileUser = Server.MapPath("~/data/user/" + username + ".json");
+                                        string fileUser = app.Server.MapPath("~/data/user/" + username + ".json");
                                         if (System.IO.File.Exists(fileUser))
                                             userJson = System.IO.File.ReadAllText(fileUser);
 
@@ -209,14 +247,48 @@ namespace io
             {
                 ;
             }
-        } // end function
+        }
+
+        //protected void testClearScript()
+        //{
+        //    string text = "", js = "";
+
+        //    using (var engine = new V8ScriptEngine())
+        //    {
+        //        string f = Server.MapPath("~/public/vue.min.js");
+        //        js = File.ReadAllText(f) + Environment.NewLine + @"
+        //            function test(html) { var template = Vue.compile(html); return template.render.toString(); }
+        //        ";
+        //        engine.Execute(js);
+        //        string html = @"<div>
+        //            <h1>{{title}}</h1>
+        //            <input type=""text"" v-model=""title"" id=""fname"" name=""fname""><br>
+        //            <special-article :article-title=""title""></special-article><br>
+        //        </div>";
+        //        text = engine.Script.test(html) as string;
+        //    }
+
+        //    HttpContext.Current.Response.Clear();
+        //    HttpContext.Current.Response.ContentType = "text/plain";
+        //    HttpContext.Current.Response.Write(text);
+        //    HttpContext.Current.Response.Flush();
+        //    HttpContext.Current.Response.Close();
+        //}
+
     }
 
-    public class oSite
+    public class Global : HttpApplication
     {
-        public int id { set; get; }
-        public string name { set; get; }
-        public string[] domains { set; get; }
-        public string[] pages { set; get; }
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            self.app_loadConfig(this);
+            self.user_loadPassword(this);
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            self.request_Router(this);
+        }
     }
+
 }
